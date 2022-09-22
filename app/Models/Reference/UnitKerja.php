c6cf1ae7cf8212db4dc04c4f5d1aafad55ba1d42
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2022-09-22 09:27:05 +0700

    remove unsued Model & Migration

---
+++ app/Models/Reference/UnitKerja.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Reference;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UnitKerja extends Model
+{
+    use HasFactory;
+}
